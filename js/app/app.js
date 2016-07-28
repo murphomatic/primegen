411763515f12685d71cadf7ff81dc180cab7fe28
Sean Murphy <smurphy@axian.com>
2016-07-28 13:22:39 -0700

    Mocked-up UI, additional styles to support ngCloak and a loading.gif spinner.

---
+++ js/app/app.js
@@ -0,0 +1,10 @@
+/* app.js
+ * Prime Generator main app file
+ * STM - 07/28/2016
+ */
+
+(function() {
+    "use strict";
+    // main application module - no requirements at this time.
+    angular.module("primeApp", []);
+})();
